ef1292631c44b41a0077d5664c61e49d27afa25d
Diego Peralta M <diego.pmz08@gmail.com>
2024-09-05 13:21:27 -0600

    Se creo el middleware validator para validar el request body

---
+++ src/middlewares/validator-middleware.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from "express";
+import z from "zod";
+
+const validatorSchema =
+  <T>(schema: z.ZodType<T>) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    const validation = schema.safeParse(req.body);
+
+    if (!validation.success || validation.error) {
+      return res.status(400).json(validation.error.errors);
+    }
+
+    return next();
+  };
+
+export default validatorSchema;
