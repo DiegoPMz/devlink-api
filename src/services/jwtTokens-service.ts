2ceec748b03de765b10a1050f1a3bbb2f9759581
Diego Peralta M <diego.pmz08@gmail.com>
2024-09-06 03:41:26 -0600

    Se creo el service jwtTokens

---
+++ src/services/jwtTokens-service.ts
@@ -0,0 +1,36 @@
+import { APP_ISSUER, PRIVATE_KEY } from "@/config/securityDetails";
+import jwt from "jsonwebtoken";
+
+type PayloadType = string | Buffer | object;
+
+export const createAccessToken = (payload: PayloadType) => {
+  return new Promise<string>((resolve, reject) => {
+    const options: jwt.SignOptions = {
+      algorithm: "HS256",
+      issuer: APP_ISSUER,
+      subject: "CURRENT_USER",
+      expiresIn: "900000",
+    };
+
+    jwt.sign(payload, PRIVATE_KEY, options, (err, token) => {
+      if (err) reject(err.message);
+      if (token) resolve(token);
+    });
+  });
+};
+
+export const createRefreshToken = (payload: PayloadType) => {
+  return new Promise<string>((resolve, reject) => {
+    const options: jwt.SignOptions = {
+      algorithm: "HS256",
+      issuer: APP_ISSUER,
+      subject: "CURRENT_USER",
+      expiresIn: "20d",
+    };
+
+    jwt.sign(payload, PRIVATE_KEY, options, (err, token) => {
+      if (err) reject(err.message);
+      if (token) resolve(token);
+    });
+  });
+};
