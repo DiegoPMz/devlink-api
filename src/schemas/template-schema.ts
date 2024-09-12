ae8d84e0d0d695425c73cd8066cd07b9a9524538
Diego Peralta M <diego.pmz08@gmail.com>
2024-09-12 02:50:05 -0600

    Se modificaron los schemas de validacion de templateSchema

--- src/schemas/template-schema.ts
+++ src/schemas/template-schema.ts
@@ -1,32 +1,56 @@
 import z from "zod";
 
-const profileLinksContentSchema = z.object({
-  platform: z
-    .string({ message: "Platform must be a string" })
-    .trim()
-    .min(1, { message: "platform can't be empty " }),
+const platformsEnumSchema = z
+  .enum(
+    [
+      "codewars",
+      "codepen",
+      "devto",
+      "facebook",
+      "freecodecamp",
+      "frontendmentor",
+      "gitlab",
+      "github",
+      "hashnode",
+      "linkedin",
+      "stackoverflow",
+      "twitter",
+      "twitch",
+      "youtube",
+    ],
+    { message: "Invalid platform. Select a valid option" },
+  )
+  .readonly();
+
+const profileLinksSchema = z.object({
+  platform: platformsEnumSchema,
   url: z
     .string({ message: "Url must be a string" })
+    .url("Invalid URL format")
     .trim()
     .min(1, { message: "url can't be empty " }),
 });
 
-export const updateTemplateSchema = z.object({
-  profile_email: z
-    .string({ message: "Email must be a string" })
-    .email({ message: "Invalid email" }),
-  profile_name: z
-    .string({ message: "Name must be a string" })
-    .min(1, { message: "Name can't be empty " }),
-  profile_last_name: z
-    .string({ message: "Last name must be a string" })
-    .min(1, { message: "Last name can't be empty " }),
-  profile_image: z
-    .string({ message: "Last name must be a string" })
-    .min(1, { message: "image can't be empty " }),
-  profile_links: z
-    .array(profileLinksContentSchema)
-    .nonempty({ message: "The user links can't be empty" }),
-});
+export const templateSchema = z
+  .object({
+    profile_email: z
+      .string({ message: "Email must be a string" })
+      .email({ message: "Invalid email" }),
+    profile_name: z
+      .string({ message: "Name must be a string" })
+      .min(1, { message: "Name can't be empty " }),
+    profile_last_name: z
+      .string({ message: "Last name must be a string" })
+      .min(1, { message: "Last name can't be empty " }),
+    profile_image: z
+      .string({ message: "Last name must be a string" })
+      .min(1, { message: "image can't be empty " }),
+    profile_links: z
+      .array(profileLinksSchema, {
+        message: "Profile links must be a valid array",
+      })
+      .nonempty({ message: "The user links can't be empty" }),
+  })
+  .strict({ message: "Incorrect request, please check the data provided" });
 
-export type UpdateTemplateType = z.infer<typeof updateTemplateSchema>;
+export type TemplateType = z.infer<typeof templateSchema>;
