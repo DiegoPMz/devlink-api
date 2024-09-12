f947501a56585fcde0edca77c831c6f3e72cfe63
Diego Peralta M <diego.pmz08@gmail.com>
2024-09-12 02:58:31 -0600

    Se agregaron las dependencias cookie-parser y @types/cookie-parser. Se configuraron las dependencias en app.ts y agregaron rutas

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
+import cookieParser from "cookie-parser";
 import express from "express";
 import authRoutes from "./routes/auth-route";
+import templateRoutes from "./routes/template-register-route";
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 // routes
-app.use("/api", authRoutes);
+app.use("/api/devlink", authRoutes);
+app.use("/api/devlink", templateRoutes);
 
 export default app;
