02686a83a1424196c236e7838f70f2c779314a28
Diego Peralta M <diego.pmz08@gmail.com>
2024-09-19 17:33:32 -0600

    Se agrego la dependencia multer y se configuro en un middleware

---
+++ src/middlewares/multer-middleware.ts
@@ -0,0 +1,24 @@
+import multer from "multer";
+
+const storage = multer.memoryStorage();
+const FILE_TYPES = ["image/jpeg", "image/png"];
+const FILE_SIZE = 1000000;
+
+const storeFile = multer({
+  storage: storage,
+  limits: {
+    files: 1,
+    fileSize: FILE_SIZE,
+    fields: 1,
+  },
+
+  fileFilter(_, file, callback) {
+    if (!FILE_TYPES.includes(file.mimetype))
+      return callback(
+        new multer.MulterError("LIMIT_FIELD_VALUE", file.fieldname),
+      );
+    return callback(null, true);
+  },
+});
+
+export default storeFile;
