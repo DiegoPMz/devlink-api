db3b2164043ae172f75d2255cd301e33d3d265f4
Diego Peralta M <diego.pmz08@gmail.com>
2024-11-17 03:37:48 -0600

    Se cambio el formato de los links en el defaultUserResponseDto

--- src/utils/responseUserModel-dtos.ts
+++ src/utils/responseUserModel-dtos.ts
@@ -12,13 +12,31 @@ export const registerResponseDto = (registerData: UserSchemaType) => ({
   createdAt: registerData.createdAt,
 });
 
-export const defaultUserResponseDto = (registerData: UserSchemaType) => ({
-  credentials: registerData.credentials,
-  email: registerData.email,
-  profile_email: registerData.profile_email ?? "",
-  profile_name: registerData.profile_name ?? "",
-  profile_last_name: registerData.profile_last_name ?? "",
-  profile_image: registerData.profile_image ?? { id: "", url: "" },
-  profile_links: registerData.profile_links ?? [],
-  profile_template: registerData.profile_template ?? "",
-});
+interface LinkFormatResponse {
+  platform: string;
+  url: string;
+  id: string;
+}
+
+export const defaultUserResponseDto = (registerData: UserSchemaType) => {
+  let formattedLinks: LinkFormatResponse[] = [];
+
+  if (registerData.profile_links && registerData.profile_links.length > 0) {
+    formattedLinks = registerData.profile_links.map((link) => ({
+      platform: link.platform,
+      url: link.url,
+      id: link._id.toString(),
+    }));
+  }
+
+  return {
+    credentials: registerData.credentials,
+    email: registerData.email,
+    profile_email: registerData.profile_email ?? "",
+    profile_name: registerData.profile_name ?? "",
+    profile_last_name: registerData.profile_last_name ?? "",
+    profile_image: registerData.profile_image ?? { id: "", url: "" },
+    profile_links: formattedLinks,
+    profile_template: registerData.profile_template ?? null,
+  };
+};
