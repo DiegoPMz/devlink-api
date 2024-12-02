153e211806b4d7ee617725cae2c81f9cbf3c2e2c
Diego Peralta M <diego.pmz08@gmail.com>
2024-12-01 19:06:33 -0600

    Se implemento handleErrorSchema en el mw validatorSchema

--- src/middlewares/validator-middleware.ts
+++ src/middlewares/validator-middleware.ts
@@ -1,3 +1,5 @@
+import createErrorResponseApp from "@/utils/error-response-app";
+import handleErrorSchema from "@/utils/handle-error-schemas";
 import { NextFunction, Request, Response } from "express";
 import z from "zod";
 
@@ -7,7 +9,8 @@ const validatorSchema =
     const validation = schema.safeParse(req.body);
 
     if (!validation.success || validation.error) {
-      return res.status(400).json(validation.error.format());
+      const formattedErrors = handleErrorSchema(validation.error.errors);
+      return res.status(400).json(createErrorResponseApp(400, formattedErrors));
     }
 
     return next();
