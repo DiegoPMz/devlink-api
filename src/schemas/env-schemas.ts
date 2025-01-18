89224036b495fb00a3ab83560fe0237273fede73
Diego Peralta M <diego.pmz08@gmail.com>
2025-01-18 04:21:08 -0600

    Se modifico un schema de las env

--- src/schemas/env-schemas.ts
+++ src/schemas/env-schemas.ts
@@ -3,15 +3,12 @@ import z from "zod";
 export const numberSchema = z.number();
 export const stringSchema = z.string().trim();
 export const dbUrlSchema = z.string().refine(
-  (url) => {
-    try {
-      const parsedUrl = new URL(url);
-      return ["mongodb:"].includes(parsedUrl.protocol);
-    } catch {
-      return false;
-    }
+  (value) => {
+    // Regex para validar la URL MongoDB + SRV
+    const regex = /^mongodb\+srv:\/\/([^:]+):([^@]+)@([^\/]+)\/\?(.+)$/;
+    return regex.test(value);
   },
   {
-    message: "Invalid database URL. Supported protocols: mongodb",
+    message: "Invalid MongoDB connection string format",
   },
 );
