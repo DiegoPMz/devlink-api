898fa4e17de7c907b0a3a6b7252804c3ee01ac5b
Diego Peralta M <diego.pmz08@gmail.com>
2025-01-22 03:05:38 -0600

    Se creo template-controller.profileImage

--- src/controllers/template-controller.ts
+++ src/controllers/template-controller.ts
@@ -1,113 +1,67 @@
 import userModel, { UserSchemaType } from "@/models/user-model";
-import { getTemplateSchema, templateSchema } from "@/schemas/template-schema";
+import { getTemplateSchema, TemplateType } from "@/schemas/template-schema";
 import { deleteCloudImage, uploadImage } from "@/services/cloudStorage-service";
 import createErrorResponseApp from "@/utils/error-response-app";
-import { errorResponse } from "@/utils/errorResponse-dto";
-import handleErrorSchema from "@/utils/handle-error-schemas";
 import { defaultUserResponseDto } from "@/utils/responseUserModel-dtos";
 import { Request, Response } from "express";
 
-type GetTemplateRequest = Request<{ id: string }>;
+export const updateTemplate = async (
+  req: Request<unknown, unknown, TemplateType>,
+  res: Response,
+) => {
+  const reqBody = req.body;
+  const token = req.user;
 
-export const updateTemplate = async (req: Request, res: Response) => {
-  try {
-    if (!req.body.data)
-      return res.status(400).json(
-        createErrorResponseApp(400, {
-          body: "Missing required 'data' in request body",
-        }),
-      );
-
-    const parseBody = JSON.parse(req.body.data);
-    const reqSchema = templateSchema.safeParse(parseBody);
-    if (reqSchema.error || !reqSchema.success) {
-      const formattedError = handleErrorSchema(reqSchema.error);
-      return res.status(400).json(createErrorResponseApp(400, formattedError));
-    }
-
-    const token = req.user;
-    if (!token)
-      return res.status(500).json(
-        createErrorResponseApp(500, {
-          service: "An unexpected error occurred. Please try again later",
-        }),
-      );
-    const userDb: UserSchemaType = await userModel
-      .findOne({ _id: token.id })
-      .catch((res: null) => res);
-
-    if (!userDb)
-      return res.status(403).json(
-        createErrorResponseApp(403, {
-          authentication: "Invalid credentials",
-        }),
-      );
-
-    const userData = reqSchema.data;
-    const reqFile = req.file;
-    let uploadImageDetails;
-
-    if (!reqFile && !userDb.profile_image?.id) {
-      return res.status(400).json(
-        createErrorResponseApp(400, {
-          user_file:
-            "The user must provide a file if none has been uploaded previously",
-        }),
-      );
-    }
-
-    if (reqFile) {
-      const uploadedImage = await uploadImage(reqFile.buffer);
-      if (!uploadedImage)
-        return res.status(500).json(
-          createErrorResponseApp(500, {
-            service: "An unexpected error occurred. Please try again later",
-          }),
-        );
-
-      const dbImageId = userDb.profile_image?.id;
-      if (dbImageId) {
-        await deleteCloudImage(dbImageId);
-      }
-
-      uploadImageDetails = {
-        id: uploadedImage.public_id,
-        url: uploadedImage.secure_url,
-      };
-    }
-
-    const newTemplateId = crypto.randomUUID();
-    const userUpdated = await userModel.findOneAndUpdate(
-      { _id: token.id },
-      {
-        ...userData,
-        profile_template: userDb.profile_template ?? newTemplateId,
-        profile_image: uploadImageDetails ?? { ...userDb.profile_image },
-      },
-      { new: true },
-    );
-
-    if (!userUpdated)
-      return res.status(500).json(
-        createErrorResponseApp(500, {
-          service: "An unexpected error occurred. Please try again later",
-        }),
-      );
-
-    return res.json({
-      ...defaultUserResponseDto(userUpdated),
-      id: userUpdated.id,
-      updatedAt: userUpdated.updatedAt,
-    });
-  } catch (err) {
-    console.error(err);
+  if (!token)
     return res.status(500).json(
       createErrorResponseApp(500, {
         service: "An unexpected error occurred. Please try again later",
       }),
     );
+
+  const userDb = await userModel
+    .findOne({ _id: token.id })
+    .catch((res: null) => res);
+  if (!userDb)
+    return res.status(403).json(
+      createErrorResponseApp(403, {
+        authentication: "Invalid credentials",
+      }),
+    );
+
+  const hasEmptyProfileImage = userDb.toObject().profile_image;
+  let templateId: undefined | string = undefined;
+
+  if (hasEmptyProfileImage?.id && hasEmptyProfileImage?.url) {
+    templateId = userDb.profile_template ?? crypto.randomUUID();
   }
+
+  const userUpdated = await userModel
+    .findOneAndUpdate(
+      { _id: token.id },
+      {
+        ...reqBody,
+        profile_template: templateId,
+      },
+      { new: true },
+    )
+    .catch(() => null);
+
+  if (!userUpdated)
+    return res.status(500).json(
+      createErrorResponseApp(500, {
+        service: "An unexpected error occurred. Please try again later",
+      }),
+    );
+
+  return res.json({
+    ...defaultUserResponseDto(userUpdated),
+    id: userUpdated.id,
+    updatedAt: userUpdated.updatedAt,
+  });
 };
+
+type GetTemplateRequest = Request<{ id: string }>;
 
 export const getTemplate = async (req: GetTemplateRequest, res: Response) => {
   try {
@@ -168,28 +122,65 @@ export const getTemplate = async (req: GetTemplateRequest, res: Response) => {
   }
 };
 
-export const templateDetails = async (req: Request, res: Response) => {
-  try {
-    const id = req.user?.id;
-    if (!id)
-      return res
-        .status(401)
-        .json(errorResponse("Authentication failed", "401"));
-
-    const currentTemplate = await userModel.findById(id);
-
-    if (!currentTemplate)
-      return res
-        .status(404)
-        .json(errorResponse("No template found for the authenticated user"));
-
-    return res.json({
-      ...defaultUserResponseDto(currentTemplate),
-      id: currentTemplate.id,
-      createdAt: currentTemplate.createdAt,
-    });
-  } catch (err) {
-    console.log(err);
-    return res.status(404).json(errorResponse("An unexpected error occurred"));
+export const profileImage = async (req: Request, res: Response) => {
+  const userImage = req.file;
+  const userToken = req.user;
+  if (!userToken) {
+    return res.status(500).json(
+      createErrorResponseApp(500, {
+        service: "An unexpected error occurred. Please try again later",
+      }),
+    );
   }
+  if (!userImage) {
+    return res.status(400).json(
+      createErrorResponseApp(400, {
+        profile_image: "Missing image",
+      }),
+    );
+  }
+
+  const userDb = await userModel
+    .findOne({ _id: userToken.id })
+    .catch(() => null);
+  if (!userDb)
+    return res.status(404).json(
+      createErrorResponseApp(403, {
+        authentication: "User not found",
+      }),
+    );
+
+  const uploadedImage = await uploadImage(userImage.buffer).catch(
+    () => undefined,
+  );
+  if (!uploadedImage)
+    return res.status(500).json(
+      createErrorResponseApp(500, {
+        service: "Image upload failed",
+      }),
+    );
+
+  if (userDb.profile_image?.id) await deleteCloudImage(userDb.profile_image.id);
+
+  const newUserImage = {
+    id: uploadedImage.public_id,
+    url: uploadedImage.secure_url,
+  };
+  const updateResponse = await userModel
+    .findByIdAndUpdate(userToken.id, { profile_image: newUserImage })
+    .catch(() => null);
+
+  if (!updateResponse) {
+    return res.status(500).json(
+      createErrorResponseApp(500, {
+        service: "Failed to update profile image. Please try again later",
+      }),
+    );
+  }
+
+  return res.status(201).json({
+    status: 201,
+    message: "Operation completed successfully",
+    data: newUserImage,
+  });
 };
--- src/routes/template-route.ts
+++ src/routes/template-route.ts
@@ -1,20 +1,29 @@
 import {
   getTemplate,
-  templateDetails,
+  profileImage,
   updateTemplate,
 } from "@/controllers/template-controller";
 import storeFile from "@/middlewares/multer-middleware";
+import validatorSchema from "@/middlewares/validator-middleware";
 import verifyToken from "@/middlewares/verifyToken-middleware";
+import { templateSchema } from "@/schemas/template-schema";
 import { Router } from "express";
 
 const route = Router();
 route.put(
   "/template",
   verifyToken,
-  storeFile.single("user_file"),
+  validatorSchema(templateSchema),
   updateTemplate,
 );
+
+route.post(
+  "/template-image",
+  verifyToken,
+  storeFile.single("profile_image"),
+  profileImage,
+);
+
 route.get("/template/:id", getTemplate);
-route.get("/template-details", verifyToken, templateDetails);
 
 export default route;
